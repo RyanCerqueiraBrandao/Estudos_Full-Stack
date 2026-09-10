e163162bf07febbd244358e68ccbd6facbef3e1c
WalterJr26 <walter.m.junior@ba.estudante.senai.br>
2026-09-10 11:15:12 -0300

    comecando calculadora

---
+++ ProjetosJava/calculadora/Main.java
@@ -0,0 +1,12 @@
+package calculadora;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        calculadora calculadora = new calculadora();
+        Scanner sc = new Scanner(System.in);
+        int opcao;
+
+    }
+}
